d699d2d0c8f6f1619d8287804699da903aa8d57d
Octavio Corral <octavio.corral@mepiel.com.mx>
2026-06-27 10:09:39 -0600

    api resend

--- src/app/api/send-email/route.js
+++ src/app/api/send-email/route.js
@@ -1,3 +1,5 @@
+export const runtime = 'edge'; // El flag para Cloudflare Pages
+
 import { Resend } from 'resend';
 import { render } from '@react-email/render';
 import { v2 as cloudinary } from 'cloudinary';
@@ -6,7 +8,7 @@ import AdminNotificationEmail from '@/emails/AdminNotificationEmail';
 import UserConfirmationEmail from '@/emails/UserConfirmationEmail';
 
 // ─── CONFIGURACIÓN DE RESEND ─────────────────────────────────────────────────
-const resend = new Resend(process.env.RESEND_API_KEY);
+    //const resend = new Resend(process.env.RESEND_API_KEY);
 
 // ─── CONFIGURACIÓN DE CLOUDINARY ─────────────────────────────────────────────
 cloudinary.config({
@@ -31,6 +33,12 @@ const sqlConfig = {
 
 export async function POST(request) {
   try {
+
+    // ─── CONFIGURACIÓN DE RESEND ─────────────────────────────────────────────────
+    //const resend = new Resend(process.env.RESEND_API_KEY);
+    const apiKey = process.env.RESEND_API_KEY || 're_placeholder_para_el_build';
+    const resend = new Resend(apiKey);
+    
     const body = await request.json();
     const { textData, files } = body;
 
